630f3c059aec889b017a09d1d872d57297d34f70
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-09-12 09:19:35 +0900

    first commit

---
+++ src/request.js
@@ -0,0 +1,13 @@
+const API_KEY = "ce2d0398931d5dfa728079ae3f2fb0f2";
+// TMDBのAPIKey
+
+export const requests = {
+  feachTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
+  feachNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  feactTopRated: `/discover/tv?api_key=${API_KEY}&languager=en-us`,
+  feactActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=28`,
+  feactComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
+  feactHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=27`,
+  feactRomanceMovies: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
+  feactDocumentMovies: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
+};
