921e4cf537ede9cedcd03d698bdc7703b6cfaf93
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-09-20 21:53:34 +0900

    全体のスタイル変更

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,12 @@ function App() {
       <Nav />
       <Banner />
       <Row
-        title="NETFLIX ORIGUINALS"
+        title="Netflix Origuinals"
         fetchUrl={requests.feachNetflixOriginals}
         isLargeRow
       />
       <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
       <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />
     </div>
